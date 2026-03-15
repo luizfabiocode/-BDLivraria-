492a6c2fa8a625a854f0112f872b32421d108ef1
luiz fabio camargo de oliveira <luizfabiocamargo@gmail.com>
2026-03-15 03:38:11 -0300

    Create insercoes.sql

---
+++ scripts/inserts/insercoes.sql
@@ -0,0 +1,5 @@
+INSERT INTO livros (titulo, autor, preco) VALUES
+('Dom Casmurro', 'Machado de Assis', 39.90),
+('O Cortiço', 'Aluísio Azevedo', 29.90),
+('Memórias Póstumas de Brás Cubas', 'Machado de Assis', 42.50),
+('Capitães da Areia', 'Jorge Amado', 35.00);
